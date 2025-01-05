1ae7468f01bec2b592d132012a12b4e8c94746d5
EthanHogan <ethan.hogan7@gmail.com>
2025-01-05 13:40:07 -0600

    shows recipes in individual cards on my cookbook page

---
+++ src/actions/getRecipes.ts
@@ -0,0 +1,18 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import { db } from "~/server/db";
+
+export async function getRecipes() {
+  // check if user is authenticated
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error("Not authorized");
+  }
+  const recipes = await db.query.recipes.findMany({
+    orderBy: (model, { asc }) => asc(model.title),
+  });
+
+  return recipes;
+}
--- src/app/my-cookbook/page.tsx
+++ src/app/my-cookbook/page.tsx
@@ -1,15 +1,88 @@
 import { auth } from "@clerk/nextjs/server";
+import { Trash } from "lucide-react";
+import Link from "next/link";
+import { Suspense } from "react";
+import { getRecipes } from "~/actions/getRecipes";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { Recipe } from "~/server/db/schema";
 
 export default async function MyCookbookPage() {
   // redirects to sign in if not authenticated
   const { userId } = await auth.protect();
 
-  console.log("userId", userId);
-
   return (
-    <div className="container">
-      <h1 className="text-5xl font-bold">My Cookbook</h1>
-      <em>List of personal recipes</em>
+    <div className="flex flex-col gap-2">
+      <div className="container">
+        <h1 className="text-5xl font-bold">My Cookbook</h1>
+        <em>List of personal recipes</em>
+      </div>
+
+      <div className="container">
+        <Suspense fallback={<div>Loading...</div>}>
+          <RecipeList />
+        </Suspense>
+      </div>
     </div>
   );
 }
+
+export async function RecipeList() {
+  const recipes = await getRecipes();
+
+  return (
+    <ul className={`flex flex-row flex-wrap gap-5`}>
+      {[
+        ...recipes,
+        ...recipes,
+        ...recipes,
+        ...recipes,
+        ...recipes,
+        ...recipes,
+        ...recipes,
+        ...recipes,
+        ...recipes,
+        ...recipes,
+        ...recipes,
+        ...recipes,
+        ...recipes,
+        ...recipes,
+        ...recipes,
+      ].map((recipe, i) => (
+        <li key={recipe.id + i + recipe.title} className="w-64">
+          <Link href={`#`}>
+            <RecipeCard recipe={recipe} />
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+const RecipeCard = ({ recipe }: { recipe: Recipe }) => {
+  const { title, description, servings_desc, cooktime_minutes } = recipe;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>
+          <div className="flex items-center justify-between">
+            <div>{title}</div>
+            <div>
+              <Trash size={16} />
+            </div>
+          </div>
+        </CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div>{servings_desc} servings</div>
+        <div>Time: {cooktime_minutes} minutes</div>
+      </CardContent>
+    </Card>
+  );
+};
