84c6129e09d2b159e81a21235cdd06b6711d0c30
EthanHogan <ethan.hogan7@gmail.com>
2025-01-05 13:38:04 -0600

    fixes bottom of pages that scroll from getting cut off

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,10 +64,10 @@ export default function RootLayout({
                 <AppSidebar />
                 <SidebarInset className="h-full peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4)-var(--header-height))]">
                   <header className="flex h-10 shrink-0 items-center gap-2 border-b px-4">
-                    <SidebarTrigger className="bg-opacity-50" />
+                    <SidebarTrigger />
                     {breadcrumbs}
                   </header>
-                  <ScrollArea className="h-[calc(100svh-theme(spacing.4)-(var(--header-height)*2))] p-3">
+                  <ScrollArea className="h-[calc(100svh+theme(spacing.2)-(var(--header-height)*2))] px-3">
                     {children}
                   </ScrollArea>
                 </SidebarInset>
